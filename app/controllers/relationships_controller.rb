307fae27b93919fc5e28571e2f05f81370505e73
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-23 13:02:41 +0900

    不要なメッセージの削除

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,6 @@ class RelationshipsController < ApplicationController
   def create
     following = current_user.follow(@user)
     if following.save
-      flash[:success] = 'ユーザーをフォローしました'
       redirect_to @user
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
@@ -15,7 +14,6 @@ class RelationshipsController < ApplicationController
   def destroy
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
       redirect_to @user
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
