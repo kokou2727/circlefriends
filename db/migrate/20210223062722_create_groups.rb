59883a4b9ab1c0bd8f49301eb2ec75eaf8288e24
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-23 15:35:43 +0900

    マイグレーションファイルの編集

--- db/migrate/20210223062722_create_groups.rb
+++ db/migrate/20210223062722_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
-
+      t.string :group_name, null: false
+      t.index :group_name, unique: true
       t.timestamps
     end
   end
