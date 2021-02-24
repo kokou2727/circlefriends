7b91e23ae79ed3b6339ab9db1f21e10adc4e8cc7
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-24 16:26:11 +0900

    画像投稿機能

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -10,16 +10,12 @@ class ChatsController < ApplicationController
     @chat = Chat.new(chat_params)
     if @chat.save
       redirect_to group_chats_path(@group)
-    else
-      @chats = @group.chats.includes(:user)
-      @group = Group.find(params[:group_id])
-      render :index
     end
   end
 
   private
 
   def chat_params
-    params.require(:chat).permit(:message).merge(user_id: current_user.id, group_id: params[:group_id])
+    params.require(:chat).permit(:message, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
