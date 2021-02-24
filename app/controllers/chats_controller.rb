98e4e2711e1126f3cfb028533442cd0484bf5e86
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-24 16:12:31 +0900

    チャット投稿

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,25 @@
 class ChatsController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
+    @chat = Chat.new
+    @chats = @group.chats.includes(:user)
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to group_chats_path(@group)
+    else
+      @chats = @group.chats.includes(:user)
+      @group = Group.find(params[:group_id])
+      render :index
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:message).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
