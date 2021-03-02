08566436b9245bd02e690fbf4d25e65d5c8b7a95
kokou2727 <kojinakasuji523025@gmail.com>
2021-03-02 16:10:19 +0900

    アイコンカラムの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
     @users = User.all
     @groups = current_user.groups
   end
-  
+
+  def edit
+    user = User.find(params[:id])
+    @image = user.user_icon
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -11,5 +25,11 @@ class UsersController < ApplicationController
 
   def add_user_to_group
     GroupUser.create(user_id: params[:user_id], group_id: params[:group_id], permit: false)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :user_icon, :profile)
   end
 end
