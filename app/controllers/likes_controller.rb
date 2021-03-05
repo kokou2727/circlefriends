9f8b05733a9b97b52c9f5bf1c427f9f8cfab5a3c
kokou2727 <kojinakasuji523025@gmail.com>
2021-03-05 16:51:05 +0900

    いいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,12 @@ class LikesController < ApplicationController
   before_action :set_post, only: [:create, :destroy]
 
   def create
-    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    @like = Like.new(user_id: current_user.id, post_id: @post.id)
+    @like.save
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    like = Like.find_by(user_id: current_user.id, post_id: @post.id)
     like.destroy
   end
 
