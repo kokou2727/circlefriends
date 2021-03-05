c9a14317a91c2a85e2cf1d159bb48d85e8eb2a7b
kokou2727 <kojinakasuji523025@gmail.com>
2021-03-05 10:52:11 +0900

    いいね機能のモデル、コントローラー、ルーティングの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-
+  has_many :likes, dependent: :destroy
   validates :text, presence: true, unless: :was_attached?
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 
   def was_attached?
     self.image.attached?
