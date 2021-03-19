744146b54d77abea1f641639d9347bce148fb1c5
kokou2727 <kojinakasuji523025@gmail.com>
2021-03-19 18:52:19 +0900

    ハッシュタグ正規表現変更

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
   def render_with_hashtags(content)
-    content.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/post/hashtag/#{word.delete("#")}"}.html_safe
+    content.gsub(/[#＃][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/post/hashtag/#{word.delete("#")}"}.html_safe
   end 
 end
