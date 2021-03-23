ab5a3122e63e7c465fd9cd0b7b25ad521f95771e
kokou2727 <kojinakasuji523025@gmail.com>
2021-03-24 03:22:02 +0900

    Revert "Revert "招待ページのroute変更""
    This reverts commit 1e9aac692d3cbe780ebcb9ec5401c8aae15c89ce.

--- app/javascript/invite.js
+++ app/javascript/invite.js
@@ -1,11 +1,13 @@
 $(document).on('turbolinks:load', function() {
-  document.getElementById("invite-btn").onclick = function() {
-    $('input:checkbox[name="group_invite_check"]:checked').each(function() {
-      $.ajax({
-        url: "/users/add_user_to_group",
-        type: "GET",
-        data: { user_id: $(this).attr('user_id'), group_id: $(this).attr('group_id') }
+  if(document.URL.match(/invite_index/)){
+    document.getElementById("invite-btn").onclick = function() {
+      $('input:checkbox[name="group_invite_check"]:checked').each(function() {
+        $.ajax({
+          url: "/users/add_user_to_group",
+          type: "GET",
+          data: { user_id: $(this).attr('user_id'), group_id: $(this).attr('group_id') }
+        });
       });
-    });
+    };
   };
 });
