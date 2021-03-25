91b1e04469a78db43695d8b1577f9afadbf17f69
kokou2727 <kojinakasuji523025@gmail.com>
2021-03-26 05:48:29 +0900

    test

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -16,11 +16,11 @@ class ChatChannel < ApplicationCable::Channel
     @user = User.find(data['user_group']['user_id'])
     if chat.save
       chat_date = chat.created_at.strftime('%p %I:%M')
-      user_image = if @user.image.attached?
-                     Rails.application.routes.url_helpers.rails_blob_path(@user.image, only_path: true)
-                   else
-                     '/assets/profile.png'
-                   end
+      if @user.image.attached?
+        user_image = Rails.application.routes.url_helpers.rails_blob_path(@user.image, only_path: true)
+      else
+        user_image = "/assets/profile.png"
+      end
       ActionCable.server.broadcast 'chat_channel', message: data['message'], user_id: data['user_group']['user_id'],
                                                    group_id: data['user_group']['group_id'], user_name: @user.name, user_image: user_image, chat_date: chat_date
     end
