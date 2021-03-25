4b6a0351aea8b3b405ab5697f4cace64e926a633
kokou2727 <kojinakasuji523025@gmail.com>
2021-03-25 18:25:18 +0900

    検索機能非同期化

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
   def render_with_hashtags(content)
-    content.gsub(/[#][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー]+/){|word| link_to word, searches_path(search: word) }.html_safe
+    content.gsub(/[#][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー]+/){|word| link_to word, searches_path(search: word), remote: true }.html_safe
   end 
 end
